8f611de34a7b683f7a10412bb07703305d7f8d56
Stephen Munro <stephen.ross.munro@gmail.com>
2021-09-04 15:33:46 +0100

    Adding list and search functions

--- src/App.js
+++ src/App.js
@@ -39,27 +39,38 @@ function App() {
         {welcome.greeting} {welcome.title}
       </h1>
       <p>Your name is {getName()}</p>
+      <Search />
+      <hr />
+      <List />
+    </div>
+  );
+}
 
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
-
-      <hr />
-
-      <ul>
-        {list.map(function (item) {
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
     </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map(function (item) {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
