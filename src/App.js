099cbd7dc5f28ffcffda66d38b31cdd1bcaa6174
Stephen Munro <stephen.ross.munro@gmail.com>
2021-09-04 12:30:36 +0100

    adding basic labels and greeting code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import * as React from "react";
 
 const title = "React";
 
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+
+function getName() {
+  return "Stephen";
+}
+
 function App() {
   // you can do something in between
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+      <p>Your name is {getName()}</p>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
